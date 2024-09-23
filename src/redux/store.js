36cb3c79a7f729d1967430a1e51ef662cf47f07a
anargha <anargharaj@gmail.com>
2024-09-23 17:10:33 +0530

    'first'

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countSlice from "./countSlice";
+
+const store=configureStore({
+    reducer:{
+      counterReducer:countSlice
+    }
+})
+export default store
